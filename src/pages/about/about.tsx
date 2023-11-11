295b489924c6470548df2beacabbfb60bd17ff4a
alpitg <alpitgajbhiye@gmail.com>
2023-11-11 18:22:18 +0530

    scss changes

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -1,4 +1,3 @@
-import "./about.scss";
 
 const AboutApp = () => {
 
@@ -6,11 +5,11 @@ const AboutApp = () => {
         <>
 
             <div className="about-app">
-                <div className="about-section">
+                <div className="about-section align-center">
                     <div className="row container">
-                        <div className="col-sm-12 lead"><h5>About</h5></div>
+                        <div className="col-sm-12 lead"><h5 className="lead">About</h5></div>
                         <div className="col-sm-12 lead"><h2>We code which impact market!</h2></div>
-                        
+
                         <div className="col-sm-10 text-muted">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fermentum pulvinar ullamcorper suspendisse ac eget. Pellentesque tempus leo in ullamcorper quis vestibulum ligula elementum ut. Congue in dignissim tincidunt ut dolor eu. Mi, eget posuere vitae sed purus nisl lorem.
                         </div>
--- src/pages/contact/contact.tsx
+++ src/pages/contact/contact.tsx
@@ -1,14 +1,13 @@
-import "./contact.scss";
+
 const ContactApp = () => {
 
     return (
         <>
-
             <div className="contact-app">
-                <div className="contact-section">
+                <div className="contact-section align-center">
                     <div className="row container">
-                        <div className="col-sm-12 lead"><h5>Contact</h5></div>
-                        <div className="col-sm-12 lead"><h2>Connect with us!</h2></div>
+                        <div className="col-sm-12"><h5 className="lead">Contact</h5></div>
+                        <div className="col-sm-12"><h2>Connect with us!</h2></div>
 
                         <div className="col-sm-10 text-muted">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fermentum pulvinar ullamcorper suspendisse ac eget. Pellentesque tempus leo in ullamcorper quis vestibulum ligula elementum ut. Congue in dignissim tincidunt ut dolor eu. Mi, eget posuere vitae sed purus nisl lorem.
