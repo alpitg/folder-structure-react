cd4fb1d92c1244a2043ca54439224321e08c3f68
alpitg <alpitgajbhiye@gmail.com>
2023-11-11 16:25:24 +0530

    about page

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -1,8 +1,23 @@
+import "./about.scss";
+
 const AboutApp = () => {
 
     return (
         <>
-            Hi About
+
+            <div className="about-app">
+                <div className="about-section">
+                    <div className="row container">
+                        <div className="col-sm-12 lead"><h5>About</h5></div>
+                        <div className="col-sm-12 lead"><h2>We code which impact market!</h2></div>
+                        
+                        <div className="col-sm-10 text-muted">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fermentum pulvinar ullamcorper suspendisse ac eget. Pellentesque tempus leo in ullamcorper quis vestibulum ligula elementum ut. Congue in dignissim tincidunt ut dolor eu. Mi, eget posuere vitae sed purus nisl lorem.
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </>
     );
 }
