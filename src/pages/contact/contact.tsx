b57474002ac574eb2fa82e1afce8da2749981936
alpitg <alpitgajbhiye@gmail.com>
2023-11-11 16:29:37 +0530

    contact

--- src/pages/contact/contact.tsx
+++ src/pages/contact/contact.tsx
@@ -1,8 +1,21 @@
+import "./contact.scss";
 const ContactApp = () => {
 
     return (
         <>
-            Hi Contact! 7774022244
+
+            <div className="contact-app">
+                <div className="contact-section">
+                    <div className="row container">
+                        <div className="col-sm-12 lead"><h5>Contact</h5></div>
+                        <div className="col-sm-12 lead"><h2>Connect with us!</h2></div>
+
+                        <div className="col-sm-10 text-muted">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fermentum pulvinar ullamcorper suspendisse ac eget. Pellentesque tempus leo in ullamcorper quis vestibulum ligula elementum ut. Congue in dignissim tincidunt ut dolor eu. Mi, eget posuere vitae sed purus nisl lorem.
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
