8a7639bf643ba30cd8b1f3d14df2b5cae9a13eeb
alpitg <alpitgajbhiye@gmail.com>
2023-12-16 15:42:45 +0530

    minor auth check added

--- src/store/sagas/auth.saga.ts
+++ src/store/sagas/auth.saga.ts
@@ -23,10 +23,7 @@ function* authenticateUser(action: IAuthenticationRequestAction) {
   } catch (error: any) {
     yield put(
       authenticateFailed({
-        error:
-          typeof error?.response?.data === "string"
-            ? [error?.response?.data]
-            : error?.response?.data,
+        error: ["Some error occured"],
         pending: false,
       })
     );
